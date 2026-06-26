93f18e36cea9b247bba0b215bc1d8962f20977c1
Abhinav Singhal <abhinavsinghal876@gmail.coml>
2026-06-27 02:01:41 +0530

    Deployed Frontend & Updated CORS

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://sin-gpt.onrender.com"],
+  }),
+);
 
 app.use("/api", chatRoutes);
 
